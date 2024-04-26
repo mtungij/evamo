248c262183792bab953fba5d9ab3f61d766831a5
james <jamesmtungi@gmail.com>
2024-04-26 11:40:02 -0700

    ..

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://evamo.co.tz/'; 
-// $config['base_url'] = 'http://localhost/eckoo/'; 
+// $config['base_url'] = 'http://localhost/evamo/'; 
 
 /*
 |--------------------------------------------------------------------------
